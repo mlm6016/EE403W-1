c254ea98107296adc6e733dfc5249b1c8b87dffc
mlm6016 <mlm6016@psu.edu>
2017-04-22 12:56:46 -0400

    Add files via upload

---
+++ EE403W - Project/SRC/eGFX/Fonts/Arial_Unicode_MS__48px__Regular__SystemDefault_1BPP/Arial_Unicode_MS__48px__Regular__SystemDefault_1BPP.h
@@ -0,0 +1,9 @@
+#include "eGFX.h" 
+
+#ifndef _ARIAL_UNICODE_MS__48PX__REGULAR__SYSTEMDEFAULT_1BPP_H
+#define _ARIAL_UNICODE_MS__48PX__REGULAR__SYSTEMDEFAULT_1BPP_H
+
+ extern const eGFX_Font Arial_Unicode_MS__48px__Regular__SystemDefault_1BPP;
+
+#endif
+
---
+++ EE403W - Project/SRC/eGFX/Fonts/Arial_Unicode_MS__48px__Regular__SystemDefault_1BPP/FontConfiguration.jsx
@@ -0,0 +1,13 @@
+/*
+  Created by JsonExSerializer
+  Assembly: lib_eGFX_Tools, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+  Type: lib_eGFX_Tools.FontBuilderConfig
+*/
+{
+    "Ascent":13056, 
+    "Descent":3328, 
+    "LineSpacing":16384, 
+    "SpacesPerTab":4, 
+    "InterCharacterSpacing":1, 
+    "CombinedOutputName":"Arial_Unicode_MS__48px__Regular__SystemDefault_1BPP"
+}
